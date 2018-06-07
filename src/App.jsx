5a15c47634ce589ab6f0f6be9f6c5c20c0cecee2
Alexko30 <rank2505@gmail.com>
2018-06-08 00:49:10 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,55 @@ class App extends Component {
     fetching: true
   }
 
+  componentDidMount() {
+    this.fetchIp();
+  }
+
+  fetchWeatherData = city => {
+    const baseUrl = `http://api.openweathermap.org`;
+    const path = `/data/2.5/weather`;
+    const appId = `dfaac986c4b6b2f4e669e75a8f9ade22`;
+    const query = `units=metric&lang=ru&appid=${appId}`;
+
+    fetch(`${baseUrl}${path}?q=${city}&${query}`)
+      .then(response => response.json())
+      .then(data => {
+        const date = new Date();
+        const time = date.getHours();
+
+        this.setState({
+          time,
+          city,
+          temperature: Math.rount(data.main.temp),
+          fetching: false
+        });
+      })
+      .catch(error => console.log(error));
+  };
+
+  fetchIp() {
+    fetch('//fregeoip.net/json/')
+      .then(response => response.json())
+      .then(({ city }) => this.fetchWeatherData(city))
+      .catch(error => console.log(error))
+  }
+
     render() {
-        return (
-            <div className="app">
-            </div>
-        );
+        const {detching, icon, time city, temperature. weatherCode} = this.state;
+
+        return fetching ?
+            <div className="app">Загрузка...</div>
+            :
+            <div className="app" data-hours={time}>
+              <WeatherIcon
+                icon = {icon}
+                weatherCode = {weatherCode}
+                time = {time} />
+
+              <WeatherDetails
+                city = {city}
+                temperature = {temperature} />
+            </div>;
     }
 }
 
